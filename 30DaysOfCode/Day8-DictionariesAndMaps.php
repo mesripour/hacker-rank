89ef1db7b19348bcbc328700fd0000c6bfdb9a91
m-mesripour <m.r.mesripour@gmail.com>
2019-05-31 12:53:38 +0430

    update DictionariesAndMaps solution

--- 30DaysOfCode/Day8-DictionariesAndMaps.php
+++ 30DaysOfCode/Day8-DictionariesAndMaps.php
@@ -1,24 +1,18 @@
 <?php
 
 // input
-$n = trim(fgets(STDIN));
+$n = fgets(STDIN);
 for ($i = 1; $i <= $n; $i++) {
     $input = explode(' ', trim(fgets(STDIN)));
     $dic[$input[0]] = $input[1];
 }
-for ($i = 1; $i <= $n; $i++) {
-    $names[] = trim(fgets(STDIN));
-}
 
 // solution
-$result = null;
-foreach ($names as $name) {
+while (!feof(STDIN)) {
+    $name = trim(fgets(STDIN));
     if (isset($dic[$name])) {
-        $result .= "$name=$dic[$name]" . PHP_EOL;
+        echo "$name=$dic[$name]" . PHP_EOL;
     } else {
-        $result .= 'Not found' . PHP_EOL;
+        echo 'Not found' . PHP_EOL;
     }
 }
-
-// output
-fwrite(STDOUT, $result);
